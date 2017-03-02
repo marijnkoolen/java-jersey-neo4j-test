35ded8d2aa7334482fd1e862c8e873cb43554eca
marijnkoolen <marijn.koolen@gmail.com>
2017-03-02 11:50:58 +0100

    Move port test to NeoRemoteShellTest

--- src/test/java/org/mytest/HelloWorldServiceTest.java
+++ src/test/java/org/mytest/HelloWorldServiceTest.java
@@ -24,26 +24,4 @@ public class HelloWorldServiceTest extends JerseyTest {
         Assert.assertTrue("Jersey says: message".equals(response));
     }
 
-    // Test used for checking port availability for Neo4j remote shell
-    public boolean portAvailable(String host, Integer port) {
-        Socket s = null;
-        try {
-            s = new Socket(host, port);
-            return false;
-        }
-        catch (Exception e) {
-            return true;
-        }
-        finally {
-            if(s != null) {
-                try {s.close();}
-                catch(Exception e){}
-            }
-        }
-    }
-
-    @Test
-    public void testPort() {
-        Assert.assertTrue(portAvailable("localhost", 5555));
-    }
 }
